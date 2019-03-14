debc7db60b125c4eb03a0e06e7014c1bab1e2d91
detoxte <48532094+detoxte@users.noreply.github.com>
2019-03-14 22:10:13 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "std_lib_facilities.h"
+#include "oppg1.h"
+
+int main() {
+	
+	cout << "Oppgave 2a)\n";
+	maxOfTwo(5, 6); cout << "\n\n";
+
+	cout << "Oppgave 2c)\n";
+	fibonacci(8); cout << "\n\n";
+
+	cout << "Oppgave 2d)\n";
+	cout << squareNumberSum(7) << "\n\n";
+
+	cout << "Oppgave 2e)\n";
+	triangleNumbersBelow(100); cout << "\n\n";
+
+	cout << "Oppgave 2f)\n";
+	cout << isPrime(37) << "\n\n";
+
+	cout << "Oppgave 2g)\n";
+	naivePrimeNumberSearch(100); cout << "\n\n";
+
+	cout << "Oppgave 2h)\n";
+	cout << findGreatestDivisor(42);
+
+	return 0;
+}
