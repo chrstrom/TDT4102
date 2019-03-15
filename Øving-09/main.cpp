8be2c75e54bbef67d5aa8ebc1deba14d11cca2d3
Christopher Strøm <chstr@stud.ntnu.no>
2019-03-15 17:44:16 +0100

    Removed old residue code

--- Øving-09/main.cpp
+++ Øving-09/main.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 #include <string>
 
-//std::ostream& operator<< (std::ostream& os, Matrix& M);
-
 int main() {
 	using std::cout;
 	using std::cin;
@@ -17,19 +15,14 @@ int main() {
 
 	//Oppgave 4;
 	Matrix A{ 2, 2 };
-	A.set(0, 0, 1.0); A.set(0, 1, 2.0);
-	A.set(1, 0, 3.0); A.set(1, 1, 4.0);
+	A.set(0, 0, 1.0); A.set(0, 1, 2.0); A.set(1, 0, 3.0); A.set(1, 1, 4.0);
 
 	Matrix B{ 2 };
-	B.set(0, 0, 4.0); B.set(0, 1, 3.0);
-	B.set(1, 0, 2.0); B.set(1, 1, 1.0);
+	B.set(0, 0, 4.0); B.set(0, 1, 3.0); B.set(1, 0, 2.0); B.set(1, 1, 1.0);
 
 	Matrix C{ 2 };
-	C.set(0, 0, 1.0); C.set(0, 1, 3.0);
-	C.set(1, 0, 1.5); C.set(1, 1, 2.0);
+	C.set(0, 0, 1.0); C.set(0, 1, 3.0); C.set(1, 0, 1.5); C.set(1, 1, 2.0);
 
-
-	
 	int choice{ -1 };
 	while (choice != 0) {
 		cout << "Meny:\n"
