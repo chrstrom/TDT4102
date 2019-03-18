e4929a6a0bdd5693f278fd9c23e20135fb9d519e
Christopher Strøm <chstr@stud.ntnu.no>
2019-03-18 15:31:35 +0100

    Oppg 2 b)

--- Øving-11/Person.cpp
+++ Øving-11/Person.cpp
@@ -4,5 +4,27 @@ std::ostream& operator<<(std::ostream& os, const Person& p) {
 	return os << p.firstname << ' ' << p.lastname << ' ';
 }
 
-
-//THIS IS A TEST COMMENT
+void insertOrdered(std::list<Person>& lst, const Person& p) {
+	std::list<Person>::const_iterator iter;
+	if (lst.empty()) {
+		lst.push_back(p);
+		return;
+	}
+	else {
+		for (iter = lst.begin(); iter != lst.end(); iter++) {
+			if ((*iter).getName() > p.getName()) {
+				lst.insert(iter, p);
+				return;
+			}
+		}
+		lst.push_back(p);
+	}
+}
+/*Skriv en funksjon for å sette inn Person-objekter i en std::list i sortert rekkefølge. Objektene skal være sortert basert
+på den alfabetiske rekkefølgen til personenes navn. Funksjonen kan for eksempel ha prototypen
+void insertOrdered(std::list<Person> &l, const Person& p);
+Test denne funksjonen ved å opprette en variabel av typen std::list<Person> og sette
+inn en rekke Person-objekter i listen ved hjelp av insertOrdered. Lag så en løkke i main()
+som skriver ut alle objektene i listen til skjermen.
+Hint: Strenger kan sammenlignes alfabetisk ved hjelp av operatorene < og >, slik at uttrykket
+"ABCD" < "BCDEF" for eksempel er sant.*/
--- Øving-11/Person.h
+++ Øving-11/Person.h
@@ -1,19 +1,23 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <list>
 
 //Oppg 2
 class Person {
 
 public:
 	friend std::ostream& operator<<(std::ostream& os, const Person& p);
-	Person() : firstname{ "" }, lastname{} {};
+	Person() : firstname{ "" }, lastname{""} {};
 	Person(std::string first, std::string last) : firstname{ first }, lastname{ last } {};
 
-	std::string getFirstName() const { return firstname; }
-	std::string getLastName() const { return lastname; }
+	//std::string getFirstName() const { return firstname; }
+	//std::string getLastName() const { return lastname; }
+	std::string getName() const { return firstname + " " + lastname; }
+
 private:
 	std::string firstname;
 	std::string lastname;
 };
 
+void insertOrdered(std::list<Person>& lst, const Person& p);
