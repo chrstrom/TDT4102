a2cd92020cca968d6bb560b62053de3ed8e7a17e
Christopher Strøm <chstr@stud.ntnu.no>
2019-03-18 15:33:45 +0100

    Removed comments

--- Øving-11/Person.cpp
+++ Øving-11/Person.cpp
@@ -20,11 +20,3 @@ void insertOrdered(std::list<Person>& lst, const Person& p) {
 		lst.push_back(p);
 	}
 }
-/*Skriv en funksjon for å sette inn Person-objekter i en std::list i sortert rekkefølge. Objektene skal være sortert basert
-på den alfabetiske rekkefølgen til personenes navn. Funksjonen kan for eksempel ha prototypen
-void insertOrdered(std::list<Person> &l, const Person& p);
-Test denne funksjonen ved å opprette en variabel av typen std::list<Person> og sette
-inn en rekke Person-objekter i listen ved hjelp av insertOrdered. Lag så en løkke i main()
-som skriver ut alle objektene i listen til skjermen.
-Hint: Strenger kan sammenlignes alfabetisk ved hjelp av operatorene < og >, slik at uttrykket
-"ABCD" < "BCDEF" for eksempel er sant.*/
